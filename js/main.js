b211844b58f7dd5ce06dcf75f41b9bf701255226
Saitama182 <100026214+Saitama182@users.noreply.github.com>
2022-12-26 20:42:48 +0300

    закончил портфолио. добавил модальное окно

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $(function(){
     var mixer = mixitup('.portfolio_content');
 });
+
+
+$('.portfolio a').fancybox({
+    caption : function( instance, item ) {
+      return $(this).parent().find('.card-text').html();
+    }
+  });
