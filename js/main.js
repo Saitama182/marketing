8483bd076575d2766847f34650ece8d73f97754a
Saitama182 <100026214+Saitama182@users.noreply.github.com>
2023-01-10 21:19:35 +0300

    header adaptive+ burger menu

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 $(function(){
-    var mixer = mixitup('.portfolio_content');
+    
 
     $('.slider-blog_inner').slick({
       dots: true,
       prevArrow: '<button type="button" class="slick-prev"><img src="../images/svg/arrow-left.svg" alt=""></button>',
       nextArrow: '<button type="button" class="slick-next"><img src="../images/svg/arrow-right.svg" alt=""></button>',
     });
+
+    $('.menu_btn').on('click' , function(){
+      $('.header_top-inner').toggleClass('header_top-inner--active');
+     
+    });
+
+    var mixer = mixitup('.portfolio_content');
+
 });
 
 
